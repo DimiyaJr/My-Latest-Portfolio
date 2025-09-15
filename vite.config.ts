5ba5c93b2e258cdbb7934d9240100604a9e8dc7f
Diman Randisa <dimanrandisa@gmail.com>
2025-09-16 00:20:13 +0530

    changed.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",  // 👈 important for GitHub Pages
+  base: "/My-Latest-Portfolio/",  // 👈 important for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
