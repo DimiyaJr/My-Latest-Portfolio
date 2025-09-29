21e27de5c27fd7318b1e2ceac043703a72d20b27
Diman Randisa <dimanrandisa@gmail.com>
2025-09-29 23:05:05 +0530

    Changed vitee.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/My-Latest-Portfolio/",  // 👈 important for GitHub Pages
+  // base: "/My-Latest-Portfolio/",  // ❌ Remove this line for Vercel
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
